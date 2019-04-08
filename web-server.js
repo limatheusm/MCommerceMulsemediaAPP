27ff8f45a8057344e0b393aab9e2cf3d596a63c6
Matheus Lima <matheus.lima@lavid.ufpb.br>
2019-04-08 17:26:03 -0300

    feat: route payment

--- web-server.js
+++ web-server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const path = require('path');
-const server = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, './public')));
-server.listen(port, () => console.log(`The server is running on http://localhost:${port}`));
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.post('/api/payment', (req, res) => {
+	if (!req.body.card) {
+		return res.json({ result: null, error: 'Card number empty' });
+	}
+	const { card } = req.body;
+
+	if (card.length != 12) {
+		return res.json({ result: -1, error: null });
+	}
+
+	return res.json({ result: card[11] === '0' ? 0 : 1, error: null });
+});
+
+app.use(express.static(path.join(__dirname, './public')));
+app.listen(port, () => console.log(`The server is running on http://localhost:${port}`));
